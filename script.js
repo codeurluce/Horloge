88bcbad266daccafa8375e75112b4100ab9c2a50
Luce <boama561@gmail.com>
2025-02-03 13:45:46 +0000

    Declaration des 1ers variables en JS

---
+++ script.js
@@ -0,0 +1,5 @@
+const body = document.querySelector("body");
+hourHand = document.querySelector(".hour");
+minuteHand = document.querySelector(".minute");
+secondHand = document.querySelector(".second");
+modeSwitch = document.querySelector(".mode-switch");
