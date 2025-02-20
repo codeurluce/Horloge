3291180907458cddbfeca45bfd240bc7a7027460
Luce <boama561@gmail.com>
2025-02-20 00:16:47 +0000

    Derniere partie

---
+++ scroll.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const scroller = document.querySelector(".scroller");
+//     const clocks = Array.from(scroller.children);
+
+//     // Duplique chaque horloge pour assurer une boucle fluide
+//     clocks.forEach(clock => {
+//         const clone = clock.cloneNode(true);
+//         scroller.appendChild(clone);
+//     });
+// });
